2f65bde2ab702f518445d70c073d9cd21ead38f1
Keeldg <154369545+Keeldg@users.noreply.github.com>
2024-06-24 20:23:46 +0300

    hw-10-start

---
+++ src/js/1-timer.js
@@ -0,0 +1,30 @@
+document.getElementById('start-btn').addEventListener('click', function () {
+  const endDate = new Date(
+    document.getElementById('datetime-picker').value
+  ).getTime();
+
+  const timer = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = endDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days < 10 ? '0' + days : days;
+    document.getElementById('hours').innerHTML =
+      hours < 10 ? '0' + hours : hours;
+    document.getElementById('minutes').innerHTML =
+      minutes < 10 ? '0' + minutes : minutes;
+    document.getElementById('seconds').innerHTML =
+      seconds < 10 ? '0' + seconds : seconds;
+
+    if (distance < 0) {
+      clearInterval(timer);
+      alert('Timer expired!');
+    }
+  }, 1000);
+});
